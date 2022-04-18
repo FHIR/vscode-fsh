ff7cc0022d984d16c53c4a62f2e9531416a085e9
Julia Afeltra <jafeltra@mitre.org>
2022-04-18 17:01:22 -0400

    Improve support for quotes around workspace folder path passed to SUSHI

--- src/SushiBuildTaskProvider.ts
+++ src/SushiBuildTaskProvider.ts
@@ -25,7 +25,7 @@ export function getSushiBuildTask(): Task {
     TaskScope.Workspace,
     'sushi', // name
     'fsh',
-    new ShellExecution('sushi "${workspaceFolder}"'),
+    new ShellExecution('sushi', ['${workspaceFolder}'], { shellQuoting: { strong: '"' } }),
     '$sushi' // problemMatcher
   );
   sushiBuildTask.presentationOptions = {
