36ee8ec855975347696dc98af7b45e8582b9ec2c
Stuart McGrigor <Stuart.McGrigor@midcentraldhb.govt.nz>
2023-04-20 15:30:40 +1200

    Added FHIR Validator as another Task that will validate SUSHI generated resources

--- src/SushiBuildTaskProvider.ts
+++ src/SushiBuildTaskProvider.ts
@@ -3,9 +3,11 @@ import { Task, TaskGroup, TaskProvider, TaskScope, ShellExecution } from 'vscode
 export class SushiBuildTaskProvider implements TaskProvider {
   constructor() {}
 
+  // Now provides a fhir validate task along side sushi
   provideTasks(): Task[] | undefined {
     const sushiBuildTask = getSushiBuildTask();
-    const tasks = [sushiBuildTask];
+    const fhirValidateTask = getFHIRValidateTask();
+    const tasks = [sushiBuildTask, fhirValidateTask];
     return tasks;
   }
 
@@ -33,4 +35,32 @@ export function getSushiBuildTask(): Task {
   };
   sushiBuildTask.group = TaskGroup.Build;
   return sushiBuildTask;
+}
+
+export function getFHIRValidateTask(): Task {
+  const fhirValidateTask = new Task(
+    { type: 'fsh', task: 'Validate FHIR' },
+    TaskScope.Workspace, // scope
+    'fhirValidator', // name
+    'fsh', // source
+    new ShellExecution(
+      'java',
+      [
+        '-jar',
+        '${workspaceFolder}/input-cache/validator_cli.jar',
+        '${workspaceFolder}/fsh-generated/resources',
+        '-version',
+        '4.0',
+        '-output-style',
+        'eslint-compact'
+      ],
+      { shellQuoting: { strong: '"' } }
+    ),
+    '$fhirValidator' // problemMatcher
+  );
+  fhirValidateTask.presentationOptions = {
+    clear: true
+  };
+  fhirValidateTask.group = TaskGroup.Build;
+  return fhirValidateTask;
 }
